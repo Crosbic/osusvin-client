d57ee953c66bcc3ce630ea0374eb5e37cbe578eb
Crosbic <thecrosbic@gmail.com>
2024-04-12 17:36:23 +0300

    доделал хедер

---
+++ src/shared/UserDropdownMenu/ui/UserDropDownStats.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { IUser } from '@/widgets/header/features/AuthModal/interfaces'
+
+const UserDropdownMenu = () => {
+  const [user, setUser] = useState<IUser>()
+
+  const { username, avatarUrl = '', pp, rank, accuracy } = user ?? {}
+
+  useEffect(() => {
+    if (localStorage.getItem('jwt')) {
+      setUser(JSON.parse(localStorage.getItem('user') ?? '{}'))
+    }
+  }, [])
+
+  const handleLogOut = () => {
+    localStorage.removeItem('jwt')
+    localStorage.removeItem('user')
+    window.location.reload()
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex flex-row gap-2 items-center cursor-pointer">
+          <div>{username}</div>
+          <Image
+            src={avatarUrl}
+            alt="avatar"
+            width="36"
+            height="36"
+            className="rounded-lg"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" sideOffset={10} className="p-2">
+        <DropdownMenuLabel>Игровая статистика</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuLabel>pp: {pp}</DropdownMenuLabel>
+        <DropdownMenuLabel>acc: {accuracy?.toFixed(2)}</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          rank: {rank?.toLocaleString('RU-ru')}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogOut}>
+          Выйти из аккаунта
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdownMenu
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,45 +5,32 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 import { ModeToggle } from '@/shared/ModeToggle/ui/ModeToggle'
-import { IUser } from '@/widgets/header/features/AuthModal/interfaces'
+import UserDropdownMenu from '@/shared/UserDropdownMenu/ui/UserDropDownStats'
 import { AuthModal } from '@/widgets/header/features/AuthModal/ui/AuthModal'
 
-import logo from '../../../../public/header-logo.png'
+import logo from '../../../../public/svin.png'
 
 export const Header = () => {
-  const [user, setUser] = useState<IUser>()
   const [token, setToken] = useState<string>()
-
-  const { username, avatarUrl = '' } = user ?? {}
 
   useEffect(() => {
     if (localStorage.getItem('jwt')) {
       setToken(localStorage.getItem('jwt') ?? '')
-      setUser(JSON.parse(localStorage.getItem('user') ?? '{}'))
     }
   }, [])
 
   return (
-    <div className="h-20 sticky flex items-center justify-between py-2 px-8 border-b-[2px]">
-      <Link href="/">
-        <Image src={logo} alt="svin logo" className="h-14 w-auto" priority />
+    <div className="h-18 sticky flex items-center justify-between py-2 px-8 border-b-[2px]">
+      <Link
+        href="/"
+        className="p-1 flex flex-row items-center justify-center gap-2 bg-primary rounded-full hover:bg-primary-foreground text-primary-foreground hover:text-primary ease-in-out duration-200"
+      >
+        <Image src={logo} alt="svin logo" className="h-9 w-auto" priority />
+        <div className="pr-2 font-bold text-3xl">o!st 2</div>
       </Link>
       <div className="flex items-center justify-between gap-4">
         <ModeToggle />
-        {token ? (
-          <div className="flex flex-row gap-2 items-center">
-            <div>{username}</div>
-            <Image
-              src={avatarUrl}
-              alt="avatar"
-              width="48"
-              height="48"
-              className="rounded-lg"
-            />
-          </div>
-        ) : (
-          <AuthModal />
-        )}
+        {token ? <UserDropdownMenu /> : <AuthModal />}
       </div>
     </div>
   )
