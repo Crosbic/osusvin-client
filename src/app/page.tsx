3003d27bda6307974cdf0b41fc3633875a0c09bd
Crosbic <thecrosbic@gmail.com>
2024-04-16 16:34:24 +0300

    fix: правильная высота и отступы всех страниц

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main>
-      <div className="m-8 flex gap-4">
-        <div>Съешь ещё этих мягких французских булок, да выпей чаю.</div>
-        <Button className="font-bold">kakashka</Button>
-      </div>
-    </main>
+    <div className="flex gap-4">
+      <div>Съешь ещё этих мягких французских булок, да выпей чаю.</div>
+      <Button className="font-bold">kakashka</Button>
+    </div>
   )
 }
